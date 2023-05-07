c8e25ec32c0668e27544de69401f7112f617bbbb
Erik Belak <erik.belak@gmail.com>
2023-05-07 13:55:31 +0200

    article CRUD 🦧

---
+++ apps/views/article.py
@@ -0,0 +1,160 @@
+import json
+import math
+from datetime import datetime
+
+from bson import ObjectId
+
+from flask import render_template, request, redirect, url_for
+
+from apps.encoders import MongoJSONEncoder
+
+
+def list_articles(db):
+    page = int(request.args.get('page', 1))
+    limit = int(request.args.get('limit', 9))
+    total = db.article.count_documents({})
+
+    items = []
+    articles = db.article.find().sort('date', -1).skip(limit * (page - 1)).limit(limit)
+    for article in articles:
+        article['text'] = article['text'][:25] + '...'
+        items.append(article)
+
+    items = json.loads(MongoJSONEncoder().encode(items))
+
+    pages = math.ceil(total / limit)
+
+    return render_template(
+        'index.html',
+        articles=items,
+        prev_page=1 if page - 1 == 0 else page - 1,
+        next_page=pages if page + 1 > pages else page + 1,
+    )
+
+
+def create_article(db):
+    data = {
+        'name': request.form['name'],
+        'date': request.form['date'],
+        'text': request.form['text_area_content'],
+        'category': request.form.getlist('category'),
+        "author_id": ObjectId("64514ab751adf62ec0bb20ce"),
+    }
+
+    db.article.insert_one(data)
+    return redirect(url_for('article_management_list'))
+
+
+def update_article(db, _id):
+    data = {
+        'name': request.form['name'],
+        'date': request.form['date'],
+        'text': request.form['text_area_content'],
+        'category': request.form.getlist('category'),
+    }
+
+    db.article.update_one({'_id': ObjectId(str(_id))}, {'$set': data})
+
+    return redirect(url_for('article_detail_detail', _id=_id))
+
+
+def update_comment_article(db, _id):
+    data = {
+        "comment": request.form['text_comment'],
+        "author": ObjectId("64514ab751adf62ec0bb20ce"),
+        "date": datetime.now()
+    }
+
+    db.article.update_one({'_id': ObjectId(str(_id))}, {"$push": {"comments": data}})
+
+    return redirect(url_for('article_detail_detail', _id=_id))
+
+
+def delete_article(db, _id):
+    db.article.delete_one({'_id': ObjectId(str(_id))})
+    return redirect(url_for('article_management_list'))
+
+
+def detail_article(db, _id):
+    pipeline = [
+        {
+            "$match": {
+                "_id": ObjectId(_id)
+            }
+        },
+        {
+            "$lookup": {
+                "from": "user",
+                "localField": "author_id",
+                "foreignField": "_id",
+                "as": "author"
+            }
+        },
+        {
+            "$unwind": {
+                "path": "$author",
+                "preserveNullAndEmptyArrays": True
+            }
+        },
+        {
+            "$unwind": {
+                "path": "$comments",
+                "preserveNullAndEmptyArrays": True
+            }
+        },
+        {
+            "$lookup": {
+                "from": "user",
+                "localField": "comments.author",
+                "foreignField": "_id",
+                "as": "comments.commenter"
+            }
+        },
+        {
+            "$unwind": {
+                "path": "$comments.commenter",
+                "preserveNullAndEmptyArrays": True
+            }
+        },
+        {
+            "$group": {
+                "_id": "$_id",
+                "name": {"$first": "$name"},
+                "date": {"$first": "$date"},
+                "text": {"$first": "$text"},
+                "author_id": {"$first": "$author_id"},
+                "category": {"$first": "$category"},
+                "author": {"$first": "$author"},
+                "comments": {
+                    "$push": {
+                        "$cond": [
+                            {"$eq": ["$comments", {}]},
+                            None,
+                            "$comments"
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            "$project": {
+                "name": 1,
+                "date": 1,
+                "text": 1,
+                "author_id": 1,
+                "category": 1,
+                "author.name": 1,
+                "author.email": 1,
+                "comments.comment": 1,
+                "comments.date": 1,
+                "comments.commenter.name": 1,
+                "comments.commenter.email": 1
+            }
+        }
+    ]
+
+    result = db.article.aggregate(pipeline)
+
+    item = next(result, None)
+
+    return render_template('detail.html', article=item)
--- mongolit/app.py
+++ mongolit/app.py
@@ -1,19 +1,17 @@
+import json
 import os
+from functools import partial
 from pathlib import Path
+
+from bson import ObjectId
 from dotenv import load_dotenv
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import pymongo
 
-from apps.views.blog import BlogDetail, BlogManagement
-
-
-def register_api(app, name):
-    blog_management = BlogManagement.as_view(f"{name}-blog_management", flask.db)
-    app.add_url_rule(f"/{name}", view_func=blog_management)
-
-    blog_detail = BlogDetail.as_view(f"{name}-blog_detail", flask.db)
-    app.add_url_rule(f"/{name}/<_id>", view_func=blog_detail)
+from apps.encoders import MongoJSONEncoder
+from apps.views.article import create_article, list_articles, detail_article, delete_article, update_article, \
+    update_comment_article
 
 
 class FlaskProject(Flask):
@@ -21,7 +19,8 @@ class FlaskProject(Flask):
         BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
         template_dir = os.path.join(BASE_DIR, 'templates')
-        super().__init__(__name__, template_folder=template_dir)
+        static_dir = os.path.join(BASE_DIR, 'static')
+        super().__init__(__name__, template_folder=template_dir, static_folder=static_dir)
 
         ENV_FILE = os.path.join(BASE_DIR, '.env')
         if os.path.exists(ENV_FILE):
@@ -29,14 +28,64 @@ class FlaskProject(Flask):
         else:
             exit(0)
 
-        client = pymongo.MongoClient(os.getenv('MONGODB'))
-        self.db = client.get_database('blog')
+        client = pymongo.MongoClient(os.getenv('MONGODB_KEY'))
+        self.db = client.get_database(os.getenv('MONGODB'))
 
 
 flask = FlaskProject(__name__)
+
 
 if __name__ == '__main__':
     flask.run()
 
 
-register_api(flask, 'blogs')
+def render_create_article():
+    return render_template('create.html')
+
+
+def render_update_article(db, _id):
+    item = db.article.find_one({'_id': ObjectId(str(_id))})
+    item = json.loads(MongoJSONEncoder().encode(item))
+
+    return render_template('update.html', article=item)
+
+
+def login():
+    return render_template('login.html')
+
+
+# retrieve all articles
+flask.add_url_rule('/articles', 'article_management_list', partial(list_articles, flask.db), methods=['GET'])
+
+# get detail article
+flask.add_url_rule(
+    '/articles/<_id>/detail', 'article_detail_detail', partial(detail_article, flask.db), methods=['GET']
+)
+
+# render create article
+flask.add_url_rule('/create', 'render_create_article', render_create_article, methods=['GET'])
+# Call create article
+flask.add_url_rule(
+    '/articles/create', 'article_management_create', partial(create_article, flask.db), methods=['POST']
+)
+
+
+# redner update article
+flask.add_url_rule(
+    '/articles/<_id>/update', 'article_detail_update', partial(render_update_article, flask.db), methods=['GET']
+)
+# call update article
+flask.add_url_rule(
+    '/articles/<_id>/update/call', 'article_detail_update_call', partial(update_article, flask.db), methods=['POST']
+)
+
+# call delete article
+flask.add_url_rule(
+    '/articles/<_id>/delete', 'article_detail_delete', partial(delete_article, flask.db), methods=['GET']
+)
+
+# call create article comment
+flask.add_url_rule(
+    '/articles/<_id>/comments', 'article_detail_create_comment',
+    partial(update_comment_article, flask.db), methods=['post']
+)
